91311ae33b1c9c05107f2bc7ce29e53f9fcde7ba
maxim.ananyev <maksim.ve@gmail.com>
2026-02-02 15:39:51 +0300

    Убрал ошибку в functions.js

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ checkLength('какой-то текст', 20);
 checkLength('какой-то недопустимо длинный текст', 20);
 
 const isPalindrom = (someText) => {
-  const cleanText = someText.replaceAll(' ', '').someText.toUpperCase();
+  const cleanText = someText.replaceAll(' ', '').toUpperCase();
   let reversedText = '';
 
   for (let i = cleanText.length - 1; i >= 0; i--) {
